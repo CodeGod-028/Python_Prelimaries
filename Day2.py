31210e81a7adafdf3e4db4029e789ddfc0f96ecd
Kratos768 <amitar20020@gmail.com>
2024-11-19 11:42:59 +0530

    Prefacing_Python

---
+++ Day2.py
@@ -0,0 +1,16 @@
+# List
+# var =["I","Love","You"]
+# print(var)
+
+# list=[]
+# print(list)
+
+# list.append(1)
+# list.append(2)
+# print(list)
+
+list=[]
+for i in range(1,7):
+   list.append(i)
+print(list) 
+
