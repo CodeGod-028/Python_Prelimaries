03754e2d81c4206f6ffc1c9eb502ac468f3cb1c2
Kratos768 <nimcet2k24@gmail.com>
2024-12-21 14:11:56 +0530

    Create DAy4.py
    Reverse_Number

---
+++ DAy4.py
@@ -0,0 +1,12 @@
+# #WAP to find reverse of a number using while loop.
+# def reverse(num):
+#     rev=0
+#     while num>0:
+#         rem=num%10   #getting remainder
+#         rev=rev*10+rem   #adding remainder to the reverse number
+#         num=num//10   #removing the last digit of the original number
+#     return rev
+
+# #testing the function
+# print(reverse(1234))   #output: 4321
+# print(reverse(987654321))   #output: 123456789
